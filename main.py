7d7e68688d944417dbc177f356702d4090a2f629
Ramina <raminapolkina@gmail.com>
2025-05-05 11:37:58 +0300

    minor chngs

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import asyncio
 import uvicorn
 from preload import *
 # from utils.create_assistant import create_assistant
-from app.api.v1.routes import create_user, demo_analysis, generate_luck_code, generate_card, check_compatibility
-from app.api.v1.routes import calculate_soul_code, calculate_soul_mission, calculate_matrix, calculate_family_error
+
 
 # async def startup() -> None:
 #     assistant_id = create_assistant()
--- preload.py
+++ preload.py
@@ -1,8 +1,11 @@
 from app.api.initial import app, api_router
 from fastapi import FastAPI
 
-root_app = FastAPI(docs_url="/app/api/v1/docs")
+from app.api.v1.routes import create_user, demo_analysis, generate_luck_code, generate_card, check_compatibility
+from app.api.v1.routes import calculate_soul_code, calculate_soul_mission, calculate_matrix, calculate_family_error
+
+root_app = FastAPI(docs_url="/docs")
 
 root_app.mount("/api/v1", app)
-app.include_router(api_router, prefix="/api/v1", tags=['routes'])
+app.include_router(api_router, prefix="/api/v1", tags=['Routes'])
 
